b81c117de201dda7ecda24bcb921309772db81a6
Andrew Cooper <andrew.cooper3@citrix.com>
2015-12-06 18:30:46 +0000

    Create hvm64 test binaries as elf32, take 2
    Experimentally, gcc/binutils from Debian Jessie (4.9.2/2.25) and CentOS
    7.1 (4.8.3/2.23) don't make valid binaries when linking x86-64 object
    files as elf32-x86-64.
    
    Issues include corrupt DWARF debugging information (as confirmed by
    gdb/readelf), and erroneously merged strings in .rodata (as confirmed by
    strings).
    
    Instead, link all binaries in their natural width, and objcopy hvm64
    binaries to elf32-x86-64 after the fact.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- config/gen.mk
+++ config/gen.mk
@@ -21,8 +21,17 @@ install: install-each-env
 
 define PERENV_build
 
+ifneq ($(1),hvm64)
+# Generic link line for most environments
 test-$(1)-$(NAME): $$(DEPS-$(1)) $$(link-$(1))
 	$$(LD) $$(LDFLAGS_$(1)) $$(DEPS-$(1)) -o $$@
+else
+# hvm64 needs linking normally, then converting to elf32-x86-64
+test-$(1)-$(NAME): $$(DEPS-$(1)) $$(link-$(1))
+	$$(LD) $$(LDFLAGS_$(1)) $$(DEPS-$(1)) -o $$@.tmp
+	objcopy $$@.tmp -O elf32-x86-64 $$@
+	rm -f $$@.tmp
+endif
 
 cfg-$(1) ?= $(defcfg-$(1))
 
