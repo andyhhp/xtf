2986115acccc542c67b51af0fe528289ff7dabe5
Andrew Cooper <andrew.cooper3@citrix.com>
2016-02-26 15:39:42 +0000

    Split common pagetable infrastructure from PAE specific infrastructure
    In preparation for the introduction of PSE paging.  In most cases, add a PAE
    prefix to existing constants.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

---
+++ include/arch/x86/page-pae.h
@@ -0,0 +1,55 @@
+/**
+ * @file include/arch/x86/page-pae.h
+ *
+ * Definitions and helpers for PAE pagetable handling.
+ */
+#ifndef XTF_X86_PAGE_PAE_H
+#define XTF_X86_PAGE_PAE_H
+
+/** PAE pagetable entries are 64 bits wide. */
+#define PAE_PTE_SZ 8
+
+/** @{ */
+/** All PAE pagetables contain 512 entries. */
+#define PAE_L1_PT_ENTRIES (PAGE_SIZE / PAE_PTE_SZ)
+#define PAE_L2_PT_ENTRIES (PAGE_SIZE / PAE_PTE_SZ)
+#define PAE_L3_PT_ENTRIES (PAGE_SIZE / PAE_PTE_SZ)
+#define PAE_L4_PT_ENTRIES (PAGE_SIZE / PAE_PTE_SZ)
+
+/* Other than PAE32_L3, which has 4 entries. */
+#define PAE32_L3_ENTRIES 4
+/** @} */
+
+#define PAE_L1_PT_SHIFT 12
+#define PAE_L2_PT_SHIFT 21
+#define PAE_L3_PT_SHIFT 30
+#define PAE_L4_PT_SHIFT 39
+
+#ifndef __ASSEMBLY__
+
+/** Integer representation of a PTE. */
+typedef uint64_t pae_intpte_t;
+
+static inline unsigned int pae_l1_table_offset(unsigned long va)
+{ return (va >> PAE_L1_PT_SHIFT) & (PAE_L1_PT_ENTRIES - 1); }
+static inline unsigned int pae_l2_table_offset(unsigned long va)
+{ return (va >> PAE_L2_PT_SHIFT) & (PAE_L2_PT_ENTRIES - 1); }
+static inline unsigned int pae_l3_table_offset(unsigned long va)
+{ return (va >> PAE_L3_PT_SHIFT) & (PAE_L3_PT_ENTRIES - 1); }
+#ifdef __x86_64__
+static inline unsigned int pae_l4_table_offset(unsigned long va)
+{ return (va >> PAE_L4_PT_SHIFT) & (PAE_L4_PT_ENTRIES - 1); }
+#endif /* __x86_64__ */
+
+#endif /* __ASSEMBLY__ */
+#endif /* XTF_X86_PAGE_PAE_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
--- include/arch/x86/page.h
+++ include/arch/x86/page.h
@@ -8,10 +8,10 @@
  */
 
 #define PAGE_SHIFT              12
-
 #define PAGE_SIZE               (_AC(1, L) << PAGE_SHIFT)
-
 #define PAGE_MASK               (~(PAGE_SIZE - 1))
+
+#include "page-pae.h"
 
 #define PAGE_ORDER_4K           0
 #define PAGE_ORDER_2M           9
@@ -22,32 +22,49 @@
 #define _PAGE_USER              0x004
 #define _PAGE_PSE               0x080
 
-#define L1_PT_SHIFT             12
-#define L2_PT_SHIFT             21
-#define L3_PT_SHIFT             30
-#define L4_PT_SHIFT             39
+#if CONFIG_PAGING_LEVELS >= 3 /* PAE Paging */
 
-#define L1_PT_ENTRIES           512
-#define L2_PT_ENTRIES           512
-#define L3_PT_ENTRIES           512
-#define L4_PT_ENTRIES           512
+#define L1_PT_SHIFT PAE_L1_PT_SHIFT
+#define L2_PT_SHIFT PAE_L2_PT_SHIFT
+#define L3_PT_SHIFT PAE_L3_PT_SHIFT
+
+#endif /* CONFIG_PAGING_LEVELS >= 3 */
+
+#if CONFIG_PAGING_LEVELS >= 4 /* PAE Paging */
+
+#define L4_PT_SHIFT PAE_L4_PT_SHIFT
+
+#endif /* CONFIG_PAGING_LEVELS >= 4 */
+
 
 #ifndef __ASSEMBLY__
 
+#if CONFIG_PAGING_LEVELS >= 3 /* PAE Paging */
+
+typedef pae_intpte_t intpte_t;
+
 static inline unsigned int l1_table_offset(unsigned long va)
-{ return (va >> L1_PT_SHIFT) & (L1_PT_ENTRIES - 1); }
+{ return pae_l1_table_offset(va); }
 static inline unsigned int l2_table_offset(unsigned long va)
-{ return (va >> L2_PT_SHIFT) & (L2_PT_ENTRIES - 1); }
+{ return pae_l2_table_offset(va); }
 static inline unsigned int l3_table_offset(unsigned long va)
-{ return (va >> L3_PT_SHIFT) & (L3_PT_ENTRIES - 1); }
-#ifdef __x86_64__
+{ return pae_l3_table_offset(va); }
+
+#endif /* CONFIG_PAGING_LEVELS >= 3 */
+
+#if CONFIG_PAGING_LEVELS >= 4 /* PAE Paging */
+
 static inline unsigned int l4_table_offset(unsigned long va)
-{ return (va >> L4_PT_SHIFT) & (L4_PT_ENTRIES - 1); }
-#endif /* __x86_64__ */
+{ return pae_l4_table_offset(va); }
 
+#endif /* CONFIG_PAGING_LEVELS >= 4 */
 
-static inline uint64_t pte_to_paddr(uint64_t pte)
+#if CONFIG_PAGING_LEVELS > 0
+
+static inline uint64_t pte_to_paddr(intpte_t pte)
 { return pte & 0x000ffffffffff000ULL; }
+
+#endif /* CONFIG_PAGING_LEVELS > 0 */
 
 #endif /* !__ASSEMBLY__ */
 
