0b6ad46369eb6f3e830c317bb763066069cf0056
Andrew Cooper <andrew.cooper3@citrix.com>
2016-06-16 12:05:01 +0000

    Introduce ASSERT() and BUILD_BUG_ON()
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- include/xtf/lib.h
+++ include/xtf/lib.h
@@ -21,6 +21,15 @@
 
 void __noreturn panic(const char *fmt, ...) __printf(1, 2);
 
+#define ASSERT(cond)                                    \
+    do { if ( !(cond) )                                 \
+            panic("ASSERT(%s) failed at %s:%u\n",       \
+                  #cond, __FILE__, __LINE__);           \
+    } while ( 0 )
+
+#define BUILD_BUG_ON(cond)                              \
+    _Static_assert(!cond, "!(" #cond ")")
+
 void heapsort(void *base, size_t nmemb, size_t size,
               int (*compar)(const void *, const void *),
               void (*swap)(void *, void *));
