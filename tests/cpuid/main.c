b88457eea65b56e8a18dedff43759b020f93b241
Andrew Cooper <andrew.cooper3@citrix.com>
2016-11-14 10:03:58 +0000

    Improvements to the cpuid utility
     * Enable viridian for HVM guests to expose more leaves
     * Identify the Xen leaves, and add subleaf logic
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- tests/cpuid/main.c
+++ tests/cpuid/main.c
@@ -18,6 +18,7 @@ static void dump_leaves(cpuid_count_fn_t cpuid_fn)
     uint64_t valid_xstate_leaves = 0;
     uint32_t max_leaf = 0, max_l7_subleaf = 0,
         max_hv_leaf = 0, max_hv2_leaf = 0, max_extd_leaf = 0;
+    uint32_t xen_first_leaf = ~0, xen_last_leaf = 0;
 
     for ( ;; )
     {
@@ -62,15 +63,52 @@ static void dump_leaves(cpuid_count_fn_t cpuid_fn)
 
         case 0x40000000U:
             max_hv_leaf = eax;
+
+            if ( ebx == XEN_CPUID_SIGNATURE_EBX &&
+                 ecx == XEN_CPUID_SIGNATURE_ECX &&
+                 edx == XEN_CPUID_SIGNATURE_EDX )
+            {
+                xen_first_leaf = leaf;
+                xen_last_leaf = eax;
+            }
+
             break;
 
         case 0x40000100U:
             max_hv2_leaf = eax;
+
+            if ( ebx == XEN_CPUID_SIGNATURE_EBX &&
+                 ecx == XEN_CPUID_SIGNATURE_ECX &&
+                 edx == XEN_CPUID_SIGNATURE_EDX )
+            {
+                xen_first_leaf = leaf;
+                xen_last_leaf = eax;
+            }
+
             break;
 
         case 0x80000000U:
             max_extd_leaf = eax;
             break;
+        }
+
+        if ( leaf >= xen_first_leaf && leaf <= xen_last_leaf )
+        {
+            /* The Xen leaves have no documented identification of max leaf. */
+
+            switch ( leaf - xen_first_leaf )
+            {
+            case 3:
+                if ( subleaf < 2 ) /* Max leaf hardcoded. */
+                {
+                    subleaf++;
+                    continue;
+                }
+                break;
+
+            case 4: /* Leaf semantics, but ??? */
+                break;
+            }
         }
 
         leaf++;
@@ -87,7 +125,9 @@ static void dump_leaves(cpuid_count_fn_t cpuid_fn)
             break;
 
         subleaf = ~0;
-        if ( leaf == 4 || leaf == 7 || leaf == 0xd )
+        if ( leaf == 4 || leaf == 7 || leaf == 0xd ||
+             ((leaf >= xen_first_leaf && leaf <= xen_last_leaf) &&
+              ((leaf - xen_first_leaf) == 3 || (leaf - xen_first_leaf) == 4)) )
             subleaf = 0;
     }
 }
