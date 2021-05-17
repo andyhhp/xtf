andyhhp/xtf

commit c92015f8ab00026f85d187d0090fc02e8ab4cfaf

build: disable PIE during linking if necessary
Starting from ee3e265688, $(CC) is used for linking. That means all
default $(CC) flags coming from distro takes effect.

On Debian Stretch, gcc contains -pie by default, which makes the final
object fail to link. We need to explicitly disable PIE when linking.
Since not all versions of gcc support -no-pie, test its availability
before adding.

Example error message:

/usr/bin/ld: /local/work/xtf.git/arch/x86/boot/head_pv64.o: relocation R_X86_64_32S against symbol `start_info' can not be used when making a shared object;
recompile with -fPIC
/usr/bin/ld: /local/work/xtf.git/arch/x86/entry_64-pv64.o: relocation R_X86_64_32S against `.text' can not be used when making a shared object; recompile with
-fPIC
/usr/bin/ld: final link failed: Nonrepresentable section on output
collect2: error: ld returned 1 exit status

Signed-off-by: Wei Liu <wei.liu2@citrix.com>
Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>

build/common.mk
@@ -50,6 +50,13 @@ obj-perarch :=
 obj-perenv  :=
 include $(ROOT)/build/files.mk
 
+
+cc-option = $(shell if [ -z "`echo 'int p=1;' | $(CC) $(1) -S -o /dev/null -x c - 2>&1`" ]; \
+			then echo y; else echo n; fi)
+
+# Disable PIE, but need to check if compiler supports it
+LDFLAGS-$(call cc-option,-no-pie) += -no-pie
+
 # Run once per environment to set up some common bits & pieces
 define PERENV_setup
 
@@ -61,7 +68,7 @@ CFLAGS_$(1) := $$(CFLAGS_$($(1)_arch)) $$(COMMON_CFLAGS-$(1)) -DCONFIG_ENV_$(1) 
 
 link-$(1) := $(ROOT)/arch/x86/link-$(1).lds
 
-LDFLAGS_$(1) := -Wl,-T,$$(link-$(1)) -nostdlib
+LDFLAGS_$(1) := -Wl,-T,$$(link-$(1)) -nostdlib $(LDFLAGS-y)
 
 # Needs to pick up test-provided obj-perenv and obj-perarch
 DEPS-$(1) = $(head-$(1)) \

commit 5ead491e36af6cb8681fc1278bd36c756ad62ac2

build: Fix build with binutils 2.36
Passing -no-pie to ld has never been correct, and binutils 2.36 now objects
properly to it.  https://sourceware.org/bugzilla/show_bug.cgi?id=27050

When this workaround was introduced, we used $(CC) to link, but this was
adjusted back to $(LD) with c/s 8486a388e2 ("build: Use LD").

However, the adjustment should always have been passed to the compile step,
not the link step.  Move the adjustment into COMMON_CFLAGS.

Fixes: c92015f8ab ("build: disable PIE during linking if necessary")
Fixes: 8486a388e2 ("build: Use LD")
Reported-by: Damien Thenot <damien.thenot@vates.fr>
Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

build/common.mk
@@ -17,6 +17,12 @@ $(foreach env,$(64BIT_ENVIRONMENTS),$(eval $(env)_arch := x86_64))
 
 COMMON_FLAGS := -pipe -I$(ROOT)/include -I$(ROOT)/arch/x86/include -MMD -MP
 
+cc-option = $(shell if [ -z "`echo 'int p=1;' | $(CC) $(1) -S -o /dev/null -x c - 2>&1`" ]; \
+			then echo y; else echo n; fi)
+
+# Disable PIE, but need to check if compiler supports it
+COMMON_CFLAGS-$(call cc-option,-no-pie) += -no-pie
+
 COMMON_AFLAGS := $(COMMON_FLAGS) -D__ASSEMBLY__
 COMMON_CFLAGS := $(COMMON_FLAGS) $(COMMON_CFLAGS-y)
 COMMON_CFLAGS += -Wall -Wextra -Werror -std=gnu99 -Wstrict-prototypes -O3 -g
@@ -37,13 +43,6 @@ defcfg-hvm   := $(ROOT)/config/default-hvm.cfg.in
 obj-perarch :=
 obj-perenv  :=
 include $(ROOT)/build/files.mk
-
-
-cc-option = $(shell if [ -z "`echo 'int p=1;' | $(CC) $(1) -S -o /dev/null -x c - 2>&1`" ]; \
-			then echo y; else echo n; fi)
-
-# Disable PIE, but need to check if compiler supports it
-LDFLAGS-$(call cc-option,-no-pie) += -no-pie
 
 # Run once per environment to set up some common bits & pieces
 define PERENV_setup