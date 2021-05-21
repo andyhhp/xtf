7c6454fb65cdbfcd92d651021b413b0ad8d085fe
Michal Orzel <michal.orzel@arm.com>
2021-05-21 08:52:55 +0200

    build: Modify build system to support multiple architectures
    Currently XTF build system is strictly made up for x86.
    Modify the build system so that it will be easier
    to add other architectures to XTF in the future.
    This is done by generalizing the common makefiles to be
    architecture independent and creating architecture
    specific makefiles.
    
    Introduce new variable SUBARCH which is the name of
    the architecture devoid of information about the bitness,
    that expands as follows:
    -on x86: x86
    -in the future on arm64/arm32: arm
    
    Modify the framework so that the test name contains
    the SUBARCH name e.g.:
    test-x86-pv64-example
    instead of:
    test-pv64-example
    
    This change will require a modification in OSSTEST
    due to the change in test naming.
    
    Signed-off-by: Michal Orzel <michal.orzel@arm.com>

--- build/gen.mk
+++ build/gen.mk
@@ -1,6 +1,6 @@
+# Architecture independent makefile for compiling tests
 
 # Sanity checking of expected parameters
-
 ifeq ($(NAME),)
 $(error NAME should be specified)
 endif
@@ -26,17 +26,17 @@ VCPUS := 1 # Default to 1 vcpu if not provided
 endif
 
 ifneq ($(VARY-CFG),)
-TEST-CFGS := $(foreach env,$(TEST-ENVS),$(foreach vary,$(VARY-CFG),test-$(env)-$(NAME)~$(vary).cfg))
+TEST-CFGS := $(foreach env,$(TEST-ENVS),$(foreach vary,$(VARY-CFG),test-$(SUBARCH)-$(env)-$(NAME)~$(vary).cfg))
 else
-TEST-CFGS := $(foreach env,$(TEST-ENVS),test-$(env)-$(NAME).cfg)
+TEST-CFGS := $(foreach env,$(TEST-ENVS),test-$(SUBARCH)-$(env)-$(NAME).cfg)
 endif
 
 .PHONY: build
-build: $(foreach env,$(TEST-ENVS),test-$(env)-$(NAME)) $(TEST-CFGS)
+build: $(foreach env,$(TEST-ENVS),test-$(SUBARCH)-$(env)-$(NAME)) $(TEST-CFGS)
 build: info.json
 
 info.json: $(ROOT)/build/mkinfo.py FORCE
-	@$(PYTHON) $< $@.tmp "$(NAME)" "$(CATEGORY)" "$(TEST-ENVS)" "$(VARY-CFG)"
+	@$(PYTHON) $< $@.tmp "$(NAME)" "$(CATEGORY)" "$(SUBARCH)" "$(TEST-ENVS)" "$(VARY-CFG)"
 	@$(call move-if-changed,$@.tmp,$@)
 
 .PHONY: install install-each-env
@@ -44,35 +44,35 @@ install: install-each-env info.json
 	@$(INSTALL_DIR) $(DESTDIR)$(xtftestdir)/$(NAME)
 	$(INSTALL_DATA) info.json $(DESTDIR)$(xtftestdir)/$(NAME)
 
-hvm64-format := $(firstword $(filter elf32-x86-64,$(shell $(OBJCOPY) --help)) elf32-i386)
-
+# Build a test for specified environment
 define PERENV_build
 
-ifneq ($(1),hvm64)
-# Generic link line for most environments
-test-$(1)-$(NAME): $$(DEPS-$(1)) $$(link-$(1))
+# If any environment needs a special compilation/linking recipe instead of
+# the default one, a custom recipe called build-$(env) e.g. build-hvm64
+# should be created in $(ROOT)/build/$(ARCH)/arch-common.mk
+
+test-$(SUBARCH)-$(1)-$(NAME): $$(DEPS-$(1)) $$(link-$(1))
+ifndef build-$(1)
+	@# Generic link line for most environments
 	$(LD) $$(LDFLAGS_$(1)) $$(DEPS-$(1)) -o $$@
 else
-# hvm64 needs linking normally, then converting to elf32-x86-64 or elf32-i386
-test-$(1)-$(NAME): $$(DEPS-$(1)) $$(link-$(1))
-	$(LD) $$(LDFLAGS_$(1)) $$(DEPS-$(1)) -o $$@.tmp
-	$(OBJCOPY) $$@.tmp -O $(hvm64-format) $$@
-	rm -f $$@.tmp
+	@# Environment specific compilation recipe
+	$(call build-$(1))
 endif
 
 cfg-$(1) ?= $(defcfg-$($(1)_guest))
 
 cfg-default-deps := $(ROOT)/build/mkcfg.py $$(cfg-$(1)) $(TEST-EXTRA-CFG) FORCE
 
-test-$(1)-$(NAME).cfg: $$(cfg-default-deps)
+test-$(SUBARCH)-$(1)-$(NAME).cfg: $$(cfg-default-deps)
 	$(PYTHON) $$< $$@.tmp "$$(cfg-$(1))" "$(VCPUS)" "$(TEST-EXTRA-CFG)" ""
 	@$(call move-if-changed,$$@.tmp,$$@)
 
-test-$(1)-$(NAME)~%.cfg: $$(cfg-default-deps) %.cfg.in
+test-$(SUBARCH)-$(1)-$(NAME)~%.cfg: $$(cfg-default-deps) %.cfg.in
 	$(PYTHON) $$< $$@.tmp "$$(cfg-$(1))" "$(VCPUS)" "$(TEST-EXTRA-CFG)" "$$*.cfg.in"
 	@$(call move-if-changed,$$@.tmp,$$@)
 
-test-$(1)-$(NAME)~%.cfg: $$(cfg-default-deps) $(ROOT)/config/%.cfg.in
+test-$(SUBARCH)-$(1)-$(NAME)~%.cfg: $$(cfg-default-deps) $(ROOT)/config/%.cfg.in
 	$(PYTHON) $$< $$@.tmp "$$(cfg-$(1))" "$(VCPUS)" "$(TEST-EXTRA-CFG)" "$(ROOT)/config/$$*.cfg.in"
 	@$(call move-if-changed,$$@.tmp,$$@)
 
@@ -80,23 +80,25 @@ test-$(1)-$(NAME)~%.cfg: $$(cfg-default-deps) $(ROOT)/config/%.cfg.in
 -include $$(DEPS-$(1):%.o=%.d)
 
 .PHONY: install-$(1) install-$(1).cfg
-install-$(1): test-$(1)-$(NAME)
+install-$(1): test-$(SUBARCH)-$(1)-$(NAME)
 	@$(INSTALL_DIR) $(DESTDIR)$(xtftestdir)/$(NAME)
 	$(INSTALL_PROGRAM) $$< $(DESTDIR)$(xtftestdir)/$(NAME)
 
-install-$(1).cfg: $(filter test-$(1)-%,$(TEST-CFGS))
+install-$(1).cfg: $(filter test-$(SUBARCH)-$(1)-%,$(TEST-CFGS))
 	@$(INSTALL_DIR) $(DESTDIR)$(xtftestdir)/$(NAME)
 	$(INSTALL_DATA) $$^ $(DESTDIR)$(xtftestdir)/$(NAME)
 
 install-each-env: install-$(1) install-$(1).cfg
 
 endef
+
+# Make a call to a function PERENV_build once per each test's environment
 $(foreach env,$(TEST-ENVS),$(eval $(call PERENV_build,$(env))))
 
 .PHONY: clean
 clean:
 	find $(ROOT) \( -name "*.o" -o -name "*.d" \) -delete
-	rm -f $(foreach env,$(TEST-ENVS),test-$(env)-$(NAME) test-$(env)-$(NAME)*.cfg)
+	rm -f $(foreach env,$(TEST-ENVS),test-$(SUBARCH)-$(env)-$(NAME) test-$(SUBARCH)-$(env)-$(NAME)*.cfg)
 
 .PHONY: %var
 %var:
--- build/x86/arch-files.mk
+++ build/x86/arch-files.mk
@@ -1,9 +1,6 @@
-# Files compiled and linked for different architectures and environments
-#
-# obj-perarch  get compiled once per architecture
-# obj-perenv   get get compiled once for each environment
-# obj-$(env)   are objects unique to a specific environment
+# Architecture specific files compiled and linked for x86
 
+# Per architecture
 obj-perarch += $(ROOT)/common/console.o
 obj-perarch += $(ROOT)/common/exlog.o
 obj-perarch += $(ROOT)/common/extable.o
@@ -17,6 +14,7 @@ obj-perarch += $(ROOT)/common/report.o
 obj-perarch += $(ROOT)/common/setup.o
 obj-perarch += $(ROOT)/common/xenbus.o
 
+# Per environment
 obj-perenv += $(ROOT)/arch/x86/decode.o
 obj-perenv += $(ROOT)/arch/x86/desc.o
 obj-perenv += $(ROOT)/arch/x86/extable.o
@@ -25,7 +23,6 @@ obj-perenv += $(ROOT)/arch/x86/hypercall_page.o
 obj-perenv += $(ROOT)/arch/x86/msr.o
 obj-perenv += $(ROOT)/arch/x86/setup.o
 obj-perenv += $(ROOT)/arch/x86/traps.o
-
 
 # HVM specific objects
 obj-hvm += $(ROOT)/arch/x86/apic.o
@@ -40,16 +37,13 @@ obj-hvm += $(ROOT)/arch/x86/x86-tss.o
 
 $(foreach env,$(HVM_ENVIRONMENTS),$(eval obj-$(env) += $(obj-hvm)))
 
-
 # PV specific objects
 obj-pv  += $(ROOT)/arch/x86/pv/traps.o
 $(foreach env,$(PV_ENVIRONMENTS),$(eval obj-$(env) += $(obj-pv)))
 
-
 # 32bit specific objects
 obj-32  += $(ROOT)/arch/x86/entry_32.o
 $(foreach env,$(32BIT_ENVIRONMENTS),$(eval obj-$(env) += $(obj-32)))
-
 
 # 64bit specific objects
 obj-64  += $(ROOT)/arch/x86/entry_64.o
