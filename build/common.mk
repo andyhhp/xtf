andyhhp/xtf

commit 8e3394a51e08affcf23cc254b92f1564b9b96972

Drop dependency on gcc-multilib
inttypes.h in particular isn't a freestanding header, and certain distros have
problems providing suitable freestanding headers anyway.  This also gets more
complicated as we start supporting other architectures.

Take the plunge and switch to entirely local headers only.

Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

build/common.mk
@@ -25,7 +25,7 @@ COMMON_AFLAGS := $(COMMON_FLAGS) -D__ASSEMBLY__
 COMMON_CFLAGS := $(COMMON_FLAGS) $(COMMON_CFLAGS-y)
 COMMON_CFLAGS += -Wall -Wextra -Werror -std=gnu99 -Wstrict-prototypes -O3 -g
 COMMON_CFLAGS += -fno-common -fno-asynchronous-unwind-tables -fno-strict-aliasing
-COMMON_CFLAGS += -fno-stack-protector -fno-pic -ffreestanding
+COMMON_CFLAGS += -fno-stack-protector -fno-pic -ffreestanding -nostdinc
 COMMON_CFLAGS += -mno-red-zone -mno-sse
 COMMON_CFLAGS += -Wno-unused-parameter -Winline
 

include/inttypes.h
@@ -1,0 +1,37 @@
+/**
+ * @file include/inttypes.h
+ *
+ * Local subset of C's inttypes.h
+ */
+#ifndef INTTYPES_H
+#define INTTYPES_H
+
+#if __SIZEOF_LONG__ == 8
+# define __PRI64  "l"
+# define __PRIPTR "l"
+#else
+# define __PRI64  "ll"
+# define __PRIPTR
+#endif
+
+#define PRId64     __PRI64  "d"
+#define PRIx64     __PRI64  "x"
+#define PRIo64     __PRI64  "o"
+#define PRIu64     __PRI64  "u"
+
+#define PRIdPTR    __PRIPTR "d"
+#define PRIoPTR    __PRIPTR "o"
+#define PRIuPTR    __PRIPTR "u"
+#define PRIxPTR    __PRIPTR "x"
+
+#endif /* INTTYPES_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

CODING_STYLE
@@ -112,15 +112,4 @@ Header files
 ------------
 
 A microkernel will not make use of system libraries, and provide local
-implementations of all required functionality.  C99 however provides
-standard headers which are explicitly safe for embedded use.  These
-should be used instead of reimplementing equivalent functionality
-locally.
-
-An incomplete list includes:
-
- * inttypes.h  (PRIx64, ...)
- * limits.h    (INT_MAX, ...)
- * stdarg.h    (va_list, ...)
- * stdbool.h   (bool, true, false)
- * stdint.h    (uint??_t ...)
+implementations of all required functionality.

docs/mainpage.dox
@@ -34,20 +34,17 @@ Environment | Guest | Width | Paging
 
 Requirements:
 - GNU Make >= 3.81
-- GNU compatible 32 and 64-bit toolchain, capable of `-std=gnu99`, `-m64`, and
-  `-m32`
-    - For Debian-based systems, the `build-essential` package is
-      sufficient. For RHEL-based systems, the `glibc-devel.i686` package is
-      generally needed beyond the default toolchain packages.
-    - Clang may be used, via `CC="clang"`
 - Python 2.6 or later
 
-Optionally:
-- A toolchain with x32 support.
-    - `hvm64` tests would prefer to use the `elf32-x86-64` format, so they
-      both load and disassemble correctly.  In the absence of x32 support,
-      `elf32-i386` will be used which will load correctly, but disassemble
-      incorrectly.
+For x86:
+    - GNU compatible 32 and 64-bit toolchain, capable of `-std=gnu99`, `-m64`,
+      and `-m32`.
+        - Clang may be used, via `CC="clang"`.
+    - Optionally, a toolchain with x32 support.
+        - `hvm64` tests would prefer to use the `elf32-x86-64` format, so they
+          both load and disassemble correctly.  In the absence of x32 support,
+          `elf32-i386` will be used which will load correctly, but disassemble
+          incorrectly.
 
 To obtain and build:
 

include/limits.h
@@ -1,0 +1,21 @@
+/**
+ * @file include/limits.h
+ *
+ * Local subset of C's limits.h
+ */
+#ifndef LIMITS_H
+#define LIMITS_H
+
+#define CHAR_BIT __CHAR_BIT__
+
+#endif /* LIMITS_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

include/stdarg.h
@@ -1,0 +1,25 @@
+/**
+ * @file include/stdarg.h
+ *
+ * Local subset of C's stdarg.h
+ */
+#ifndef STDARG_H
+#define STDARG_H
+
+typedef __builtin_va_list va_list;
+#define va_start(v, l)  __builtin_va_start(v, l)
+#define va_end(v)       __builtin_va_end(v)
+#define va_arg(v, l)    __builtin_va_arg(v, l)
+#define va_copy(d, s)   __builtin_va_copy(d, s)
+
+#endif /* STDARG_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

include/stdbool.h
@@ -1,0 +1,23 @@
+/**
+ * @file include/stdbool.h
+ *
+ * Local subset of C's stdbool.h
+ */
+#ifndef STDBOOL_H
+#define STDBOOL_H
+
+typedef _Bool bool;
+#define true	1
+#define false	0
+
+#endif /* STDBOOL_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

include/stddef.h
@@ -1,0 +1,26 @@
+/**
+ * @file include/stddef.h
+ *
+ * Local subset of C's stddef.h
+ */
+#ifndef STDDEF_H
+#define STDDEF_H
+
+typedef __SIZE_TYPE__       size_t;
+typedef __PTRDIFF_TYPE__    ptrdiff_t;
+
+#define NULL ((void *)0)
+
+#define offsetof(t, m) __builtin_offsetof(t, m)
+
+#endif /* STDDEF_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */

commit ff160740d9114293f10bb34355b993f54f8e6659

build: Move -nostdinc from COMMON_CFLAGS to COMMON_FLAGS
It needs to find its way into COMMON_AFLAGS too.

Also fix a latent bug introduced at the same time.  PRIx32 is used by
PSE_PRIpte.

Fixes: 8e3394a51e08 ("Drop dependency on gcc-multilib")
Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

build/common.mk
@@ -17,7 +17,7 @@ $(foreach env,$(64BIT_ENVIRONMENTS),$(eval $(env)_arch := x86_64))
 
 comma := ,
 
-COMMON_FLAGS := -pipe -I$(ROOT)/include -I$(ROOT)/arch/x86/include -MMD -MP
+COMMON_FLAGS := -pipe -nostdinc -I$(ROOT)/include -I$(ROOT)/arch/x86/include -MMD -MP
 
 cc-option = $(shell if [ -z "`echo 'int p=1;' | $(CC) $(1) -c -o /dev/null -x c - 2>&1`" ]; \
 			then echo y; else echo n; fi)
@@ -36,7 +36,7 @@ COMMON_AFLAGS := $(COMMON_FLAGS) $(COMMON_AFLAGS-y) -D__ASSEMBLY__
 COMMON_CFLAGS := $(COMMON_FLAGS) $(COMMON_CFLAGS-y)
 COMMON_CFLAGS += -Wall -Wextra -Werror -std=gnu99 -Wstrict-prototypes -O3 -g
 COMMON_CFLAGS += -fno-common -fno-asynchronous-unwind-tables -fno-strict-aliasing
-COMMON_CFLAGS += -fno-stack-protector -fno-pic -ffreestanding -nostdinc
+COMMON_CFLAGS += -fno-stack-protector -fno-pic -ffreestanding
 COMMON_CFLAGS += -mno-red-zone -mno-sse
 COMMON_CFLAGS += -Wno-unused-parameter -Winline
 

include/inttypes.h
@@ -14,6 +14,11 @@
 # define __PRIPTR
 #endif
 
+#define PRId32              "d"
+#define PRIx32              "x"
+#define PRIo32              "o"
+#define PRIu32              "u"
+
 #define PRId64     __PRI64  "d"
 #define PRIx64     __PRI64  "x"
 #define PRIo64     __PRI64  "o"